49ff8034cf77035a5420243a556098ffe2e17852
Mildred Awuor <otieno.awuor.m@gmail.com>
2021-01-21 09:57:02 +0300

    changed cat name to lower case

--- catsanddogs/src/main/java/com/lambdaschool/catsanddogs/models/Cat.java
+++ catsanddogs/src/main/java/com/lambdaschool/catsanddogs/models/Cat.java
@@ -29,7 +29,7 @@ public class Cat
 
     public String getCatname()
     {
-        return catname.toUpperCase();
+        return catname.toLowerCase();
     }
 
     public void setCatname(String catname)
